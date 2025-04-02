a18030708546a9c6c50cee4ed73e507f13403b10
edison <edison_20_04@hotmail.com>
2025-04-02 16:00:37 -0500

    Hidden Card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,23 +1,36 @@
-import type { Card as CardType } from '../game/deck'
+import type { Card as CardType, Color } from '../game/deck'
 
 interface CardProps {
-  card: CardType
+  card?: CardType
   playerTurn?: number
   playerId?: string
   discardCard?: (card: CardType, playerTurn: number, playerId: string) => void
 }
 
 export function Card({ card, playerTurn, playerId, discardCard }: Readonly<CardProps>) {
+  
+  if (!card) {
+    return <div className='cardComponent' style={{ background: 'gray', cursor: 'context-menu'}}>🤔❓</div>
+  }
 
   const { value, color } = card
   const fontColor = color === 'blue' ? 'white' : 'black'
 
+  const cardStyles: {background: Color | undefined, color: string, cursor?: string} = {
+    background: color,
+    color: fontColor
+  }
+  
+  if (!discardCard){
+    cardStyles.cursor = 'context-menu'
+  }
+  
   return (
     <button
       onClick={() => discardCard?.(card, playerTurn ?? 0, playerId ?? '')}
       data-key={card.key}
       className='cardComponent'
-      style={{ background: color, color: fontColor }}>
+      style={cardStyles}>
       {value}
     </button>
   )
