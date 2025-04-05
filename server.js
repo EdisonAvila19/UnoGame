c7ec3174882edc617766a9489fc3a22c90041dcb
edison <edison_20_04@hotmail.com>
2025-04-05 18:25:44 -0500

    fix server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from 'express'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
-// import cors from 'cors'
-import 'dotenv/config'
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+// import cors from 'cors';
+import 'dotenv/config';
 
-const app = express()
-const server = createServer(app)
+const app = express();
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
   },
-})
+});
 
 let gameState = {
   hands: [], // Card[][]
@@ -23,47 +23,48 @@ let gameState = {
   penalty: 0, // number
   gameDirection: 'Clockwise', // 'Clockwise' | 'CounterClockwise'
   players: [], // Player[]
-}
+};
 
 io.on('connection', (socket) => {
   console.log(`Jugador conectado: ${socket.id}`);
 
   socket.on('joinGame', (playerName) => {
     if (gameState.players.length < 4) {
-      const player = gameState.players.find(p => p.id === socket.id)
-      if (!player) 
-        gameState.players.push({ id: socket.id, name: playerName, turn: 0 })
-        
+      const player = gameState.players.find(p => p.id === socket.id);
+      if (!player) {
+        gameState.players.push({ id: socket.id, name: playerName, turn: 0 });
+      }
+
       gameState.players.forEach((player, index) => {
-        player.turn = index
+        player.turn = index;
       })
 
-      socket.emit('gameState', {...gameState, id: socket.id}) 
-      io.emit('updatePlayers', gameState.players)
-      console.log(gameState)
+      socket.emit('gameState', {...gameState, id: socket.id}) ;
+      io.emit('updatePlayers', gameState.players);
+      console.log(gameState);
     }
   })
 
   socket.on('startGame', (data) => {
-    gameState = { ...data }
+    gameState = { ...data };
 
-    let gameStart = false
-    let getRandomTurn = 0
+    let gameStart = false;
+    let getRandomTurn = 0;
     if (gameState.players.length > 1) {
-      gameStart = true
-      getRandomTurn = Math.floor(Math.random() * gameState.players.length)
+      gameStart = true;
+      getRandomTurn = Math.floor(Math.random() * gameState.players.length);
     }
 
-    io.emit('gameState', {...gameState, turn: getRandomTurn, gameStart})
+    io.emit('gameState', {...gameState, turn: getRandomTurn, gameStart});
   })
 
   socket.on('endTurn', (data) => {
-    gameState = { ...gameState, ...data }
-    io.emit('gameState', gameState)
+    gameState = { ...gameState, ...data };
+    io.emit('gameState', gameState);
   })
 
   socket.on('drawCard', ({ hands, remainingDeck, discardPile }) => {
-    io.emit('gameState', { ...gameState, hands, remainingDeck, discardPile })
+    io.emit('gameState', { ...gameState, hands, remainingDeck, discardPile });
   })
 
   socket.on('disconnect', () => {
@@ -77,12 +78,12 @@ io.on('connection', (socket) => {
       penalty: 0,
       gameDirection: 'Clockwise',
       players: [],
-    }
-    // gameState.players = gameState.players.filter((player) => player.id !== socket.id)
-    io.emit('updatePlayers', gameState.players)
-    io.emit('gameState', { ...gameState })
+    };
+    // gameState.players = gameState.players.filter((player) => player.id !== socket.id);
+    io.emit('updatePlayers', gameState.players);
+    io.emit('gameState', { ...gameState });
     console.log(`Jugador desconectado: ${socket.id}`);
   })
 })
 
-server.listen(process.env.SERVER_PORT, '0.0.0.0', () => console.log(`Servidor corriendo en el puerto ${process.env.SERVER_PORT}`))
+server.listen(process.env.SERVER_PORT, '0.0.0.0', () => console.log(`Servidor corriendo en el puerto ${process.env.SERVER_PORT}`));
