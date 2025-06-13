bda009a5b2f8e136a849108e5bc9489747f3c80a
edison <edison_20_04@hotmail.com>
2025-06-12 21:27:45 -0500

    refactor Frontend-Backend

--- src/services/socket.ts
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client'
-
-export const socket = io(import.meta.env.VITE_SERVER_URL || 'http://localhost:3001', {
-  transports: ['websocket'],
-  withCredentials: true
-})
