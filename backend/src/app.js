1c10db1712096441cc92eb0bd82de0a60de15226
edison <edison_20_04@hotmail.com>
2025-06-16 17:38:48 -0500

    remove cl message

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,10 @@ import { createServer } from 'node:http'
 import { Server } from 'socket.io'
 import cors from 'cors'
 
-console.log('Iniciando archivo app.js')
-
 const allowedOrigins = [
   'https://unodev.netlify.app',
   'http://localhost:5173'
 ]
-
-// const allowedOrigins = '*'
 
 const app = express()
 const server = createServer(app)
